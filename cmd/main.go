fc88ca8118fd57d60e9464d4ec84f527d86b5f26
Nikolai Zhogin <nickzhog@gmail.com>
2023-01-25 19:05:21 +0300

    cmd implementation

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/nickzhog/gophermart/internal/config"
+	"github.com/nickzhog/gophermart/internal/repositories"
+	"github.com/nickzhog/gophermart/internal/web"
+	"github.com/nickzhog/gophermart/pkg/logging"
+)
 
+func main() {
+	logger := logging.GetLogger()
+	cfg := config.GetConfig()
+
+	reps := repositories.GetRepositories(logger, cfg)
+
+	web.StartServer(logger, cfg, reps)
 }
