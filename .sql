4211a2765b91fe3ba1416c15b90a3a098a350ca3
Nikolai Zhogin <nickzhog@gmail.com>
2023-01-26 17:54:52 +0300

    sql tables add

---
+++ .sql
@@ -0,0 +1,27 @@
+CREATE TABLE IF NOT EXIST public.users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    login TEXT NOT NULL,
+    password_hash TEXT NOT NULL, 
+    balance TEXT NOT NULL,
+    withdrawn_amount TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXIST public.orders (
+    id TEXT PRIMARY KEY,
+    user_id UUID NOT NULL,
+    status text NOT NULL DEFAULT 'NEW',
+    accrual TEXT NOT NULL,
+    sum TEXT NOT NULL,
+    upload_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	constraint user_id foreign key (user_id) references public.users (id)
+);
+
+CREATE TABLE IF NOT EXIST public.withdrawals (
+    id TEXT PRIMARY KEY,
+    user_id UUID NOT NULL,
+    sum TEXT NOT NULL,
+    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	constraint user_id foreign key (user_id) references public.users (id)
+);
+
