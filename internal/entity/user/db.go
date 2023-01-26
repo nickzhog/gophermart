821b95fbd8a22493a20fc2e536b94b43713cb682
Nikolai Zhogin <nickzhog@gmail.com>
2023-01-26 22:32:41 +0300

    user db implementation

--- internal/entity/user/db.go
+++ internal/entity/user/db.go
@@ -2,7 +2,11 @@ package user
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"strconv"
 
+	"github.com/jackc/pgconn"
 	"github.com/nickzhog/gophermart/internal/postgres"
 	"github.com/nickzhog/gophermart/pkg/logging"
 )
@@ -12,20 +16,88 @@ type repository struct {
 	logger *logging.Logger
 }
 
-func (r *repository) Create(ctx context.Context, usr User) (User, error) {
-	panic("not implemented") // TODO: Implement
+func (r *repository) Create(ctx context.Context, usr *User) error {
+	q := `
+		INSERT INTO public.users 
+		    (login, password_hash) 
+		VALUES 
+		    ($1, $2) 
+		RETURNING id
+	`
+	err := r.client.QueryRow(ctx, q, usr.Login, usr.PasswordHash).Scan(&usr.ID)
+	if err != nil {
+		var pgErr *pgconn.PgError
+		if errors.As(err, &pgErr) {
+			pgErr = err.(*pgconn.PgError)
+			newErr := fmt.Errorf(fmt.Sprintf("SQL Error: %s, Detail: %s, Where: %s, Code: %s, SQLState: %s",
+				pgErr.Message, pgErr.Detail, pgErr.Where, pgErr.Code, pgErr.SQLState()))
+			r.logger.Error("err:", newErr.Error())
+		}
+		r.logger.Error("err:", err.Error())
+	}
+	return err
 }
 
 func (r *repository) FindByLogin(ctx context.Context, login string) (User, error) {
-	panic("not implemented") // TODO: Implement
+	q := `
+	SELECT
+	 id, password_hash, balance, withdrawn_amount
+	FROM public.users WHERE login = $1
+	`
+
+	var usr User
+	err := r.client.QueryRow(ctx, q, login).
+		Scan(&usr.ID, &usr.PasswordHash, &usr.Balance)
+	if err != nil {
+		return User{}, err
+	}
+
+	usr.BalanceFloat, err = strconv.ParseFloat(usr.Balance, 64)
+	if err != nil {
+		return User{}, err
+	}
+
+	return usr, nil
 }
 
 func (r *repository) FindByID(ctx context.Context, id string) (User, error) {
-	panic("not implemented") // TODO: Implement
+	q := `
+	SELECT
+	 login, password_hash, balance, withdrawn_amount
+	FROM public.users WHERE id = $1
+	`
+
+	var usr User
+	err := r.client.QueryRow(ctx, q, id).
+		Scan(&usr.Login, &usr.PasswordHash, &usr.Balance)
+	if err != nil {
+		return User{}, err
+	}
+
+	usr.BalanceFloat, err = strconv.ParseFloat(usr.Balance, 64)
+	if err != nil {
+		return User{}, err
+	}
+
+	return usr, nil
 }
 
-func (r *repository) Update(ctx context.Context, usr User) error {
-	panic("not implemented") // TODO: Implement
+func (r *repository) Update(ctx context.Context, usr *User) error {
+
+	usr.Balance = fmt.Sprintf("%g", usr.BalanceFloat)
+
+	q := `
+		UPDATE public.users 
+		SET
+		 balance = $1, 
+		 withdraw_amount = $2
+		WHERE id = $3
+	`
+
+	_, err := r.client.Exec(ctx, q,
+		usr.Balance, usr.ID)
+
+	return err
 }
 
 func NewRepository(client postgres.Client, logger *logging.Logger) Repository {
@@ -35,8 +107,7 @@ func NewRepository(client postgres.Client, logger *logging.Logger) Repository {
 		create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 		login TEXT NOT NULL,
 		password_hash TEXT NOT NULL, 
-		balance TEXT NOT NULL,
-		withdrawn_amount TEXT NOT NULL
+		balance TEXT NOT NULL DEFAULT '0'
 	);
 	`
 	_, err := client.Exec(context.TODO(), q)
--- internal/entity/user/user.go
+++ internal/entity/user/user.go
@@ -2,30 +2,19 @@ package user
 
 import (
 	"context"
-	"strconv"
 )
 
 type User struct {
-	ID                   string  `json:"id,omitempty"`
-	Login                string  `json:"login,omitempty"`
-	PasswordHash         string  `json:"password_hash,omitempty"`
-	WithdrawnAmount      string  `json:"withdrawn_amount,omitempty"` // сумма использованных за весь период регистрации баллов
-	WithdrawnAmountFloat float64 `json:"withdrawn_amount_float,omitempty"`
-	Balance              string  `json:"balance,omitempty"`
-	BalanceFloat         float64 `json:"balance_float,omitempty"`
+	ID           string  `json:"id,omitempty"`
+	Login        string  `json:"login,omitempty"`
+	PasswordHash string  `json:"password_hash,omitempty"`
+	Balance      string  `json:"balance,omitempty"`
+	BalanceFloat float64 `json:"balance_float,omitempty"`
 }
 
 type Repository interface {
-	Create(ctx context.Context, usr User) (User, error)
+	Create(ctx context.Context, usr *User) error
 	FindByLogin(ctx context.Context, login string) (User, error)
 	FindByID(ctx context.Context, id string) (User, error)
-	Update(ctx context.Context, usr User) error
-}
-
-func (u *User) ParseFloats() (err error) {
-	if u.WithdrawnAmountFloat, err = strconv.ParseFloat(u.WithdrawnAmount, 64); err != nil {
-		return
-	}
-	u.BalanceFloat, err = strconv.ParseFloat(u.Balance, 64)
-	return
+	Update(ctx context.Context, usr *User) error
 }
