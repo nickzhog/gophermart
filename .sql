a5b11cbb61065233adb9f5e9ca8ceae03b424dab
Nikolai Zhogin <nickzhog@gmail.com>
2023-01-27 12:11:54 +0300

    tables fix

--- .sql
+++ .sql
@@ -3,25 +3,24 @@ CREATE TABLE IF NOT EXISTS public.users (
     create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     login TEXT NOT NULL,
     password_hash TEXT NOT NULL, 
-    balance TEXT NOT NULL,
-    withdrawn_amount TEXT NOT NULL
+    balance TEXT NOT NULL DEFAULT '0'
 );
 
 CREATE TABLE IF NOT EXISTS public.orders (
     id TEXT PRIMARY KEY,
     user_id UUID NOT NULL,
-    status text NOT NULL DEFAULT 'NEW',
+    status TEXT NOT NULL DEFAULT 'NEW',
     accrual TEXT NOT NULL,
     sum TEXT NOT NULL,
-    upload_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	constraint user_id foreign key (user_id) references public.users (id)
+    upload_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	constraint user_id FOREIGN KEY (user_id) REFERENCES public.users (id)
 );
 
 CREATE TABLE IF NOT EXISTS public.withdrawals (
-    id TEXT PRIMARY KEY,
+    id UUID PRIMARY KEY,
     user_id UUID NOT NULL,
     sum TEXT NOT NULL,
-    processed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	constraint user_id foreign key (user_id) references public.users (id)
+	processed_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	constraint user_id FOREIGN KEY (user_id) REFERENCES public.users (id)
 );
 
