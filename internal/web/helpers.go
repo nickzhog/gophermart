590db7693faf7a1f613af44c9df5529aa585ca83
Nikolai Zhogin <nickzhog@gmail.com>
2023-02-06 16:23:10 +0300

    logging for all answers

--- internal/web/helpers.go
+++ internal/web/helpers.go
@@ -2,11 +2,13 @@ package web
 
 import "net/http"
 
-func writeError(w http.ResponseWriter, err string, code int) {
+func (h *HandlerData) writeError(w http.ResponseWriter, err string, code int) {
+	h.Logger.Tracef("write error: err(%s), code(%v)", err, code)
 	http.Error(w, err, code)
 }
 
-func writeAnswer(w http.ResponseWriter, ans string, code int) {
+func (h *HandlerData) writeAnswer(w http.ResponseWriter, ans string, code int) {
+	h.Logger.Tracef("write answer: ans(%s), code(%v)", ans, code)
 	w.WriteHeader(code)
 	w.Write([]byte(ans))
 }
--- internal/web/user_handlers.go
+++ internal/web/user_handlers.go
@@ -16,28 +16,28 @@ import (
 func (h *HandlerData) registerHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	authData, err := user.ParseAuthRequest(body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	usr, err := user.NewUser(authData.Login, authData.Password)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	_, err = h.User.FindByLogin(r.Context(), authData.Login)
 	if err == nil {
-		writeError(w, "login already used", http.StatusConflict)
+		h.writeError(w, "login already used", http.StatusConflict)
 		return
 	}
 
 	err = h.User.Create(r.Context(), &usr)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
@@ -47,33 +47,33 @@ func (h *HandlerData) registerHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	s, err := h.Session.Create(r.Context(), usr.ID)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	session.PutSessionIDInCookie(w, s.ID)
-	writeAnswer(w, "regiteration complete", http.StatusOK)
+	h.writeAnswer(w, "regiteration complete", http.StatusOK)
 }
 
 // аутентификация пользователя
 func (h *HandlerData) loginHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	authData, err := user.ParseAuthRequest(body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	usr, err := h.User.FindByLogin(r.Context(), authData.Login)
 	if err != nil {
-		writeError(w, "user not found", http.StatusUnauthorized)
+		h.writeError(w, "user not found", http.StatusUnauthorized)
 	}
 
 	err = bcrypt.CompareHashAndPassword([]byte(usr.PasswordHash), []byte(authData.Password))
 	if err != nil {
-		writeError(w, "wrong password", http.StatusUnauthorized)
+		h.writeError(w, "wrong password", http.StatusUnauthorized)
 		return
 	}
 
@@ -83,19 +83,19 @@ func (h *HandlerData) loginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	s, err := h.Session.Create(r.Context(), usr.ID)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	session.PutSessionIDInCookie(w, s.ID)
 
-	writeAnswer(w, "authentication complete", http.StatusOK)
+	h.writeAnswer(w, "authentication complete", http.StatusOK)
 }
 
 // загрузка пользователем номера заказа для расчёта
 func (h *HandlerData) newOrderHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
@@ -103,27 +103,27 @@ func (h *HandlerData) newOrderHandler(w http.ResponseWriter, r *http.Request) {
 	order, err := order.NewOrder(string(body), usrID)
 	if err != nil {
 		h.Logger.Errorf("bad order: %s, %s", string(body), err.Error())
-		writeError(w, err.Error(), http.StatusUnprocessableEntity)
+		h.writeError(w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
 
 	if o, err := h.Order.FindByID(r.Context(), string(body)); err == nil {
 		if o.UserID == usrID {
-			writeAnswer(w, "already have that order", http.StatusOK)
+			h.writeAnswer(w, "already have that order", http.StatusOK)
 			return
 		} else {
-			writeError(w, "order for another user", http.StatusConflict)
+			h.writeError(w, "order for another user", http.StatusConflict)
 			return
 		}
 	}
 
 	err = h.Order.Create(r.Context(), &order)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	writeAnswer(w, "success", http.StatusAccepted)
+	h.writeAnswer(w, "success", http.StatusAccepted)
 }
 
 // получение списка загруженных пользователем номеров заказов, статусов их обработки и информации о начислениях
@@ -131,18 +131,18 @@ func (h *HandlerData) getOrdersHandler(w http.ResponseWriter, r *http.Request) {
 	usrID := user.GetUserIDFromRequest(r)
 	orders, err := h.Order.FindForUser(r.Context(), usrID)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if len(orders) < 1 {
-		writeAnswer(w, "no orders", http.StatusNoContent)
+		h.writeAnswer(w, "no orders", http.StatusNoContent)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(orders)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
@@ -152,7 +152,7 @@ func (h *HandlerData) balanceHandler(w http.ResponseWriter, r *http.Request) {
 	usrID := user.GetUserIDFromRequest(r)
 	withdrawals, err := h.Withdrawal.FindForUser(r.Context(), usrID)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	withdrawn := withdrawal.SumForWithdrawals(withdrawals)
@@ -166,7 +166,7 @@ func (h *HandlerData) balanceHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(m)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
@@ -175,12 +175,12 @@ func (h *HandlerData) balanceHandler(w http.ResponseWriter, r *http.Request) {
 func (h *HandlerData) withdrawActionHandler(w http.ResponseWriter, r *http.Request) {
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusBadRequest)
+		h.writeError(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	wReq, err := withdrawal.ParseWithdrawalRequest(body)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusUnprocessableEntity)
+		h.writeError(w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
 	usrID := user.GetUserIDFromRequest(r)
@@ -191,28 +191,28 @@ func (h *HandlerData) withdrawActionHandler(w http.ResponseWriter, r *http.Reque
 	withdrawn := withdrawal.SumForWithdrawals(withdrawals)
 	balance := order.AccrualSumForOrders(orders) - withdrawn
 	if balance < wReq.Sum {
-		writeError(w, "not enough balance", http.StatusPaymentRequired)
+		h.writeError(w, "not enough balance", http.StatusPaymentRequired)
 		return
 	}
 
 	wdl, err := withdrawal.NewWithdrawal(wReq.Order, usrID, wReq.Sum)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusUnprocessableEntity)
+		h.writeError(w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
 	_, err = h.Withdrawal.FindByID(r.Context(), wdl.ID)
 	if err == nil {
-		writeError(w, "order already used", http.StatusConflict)
+		h.writeError(w, "order already used", http.StatusConflict)
 		return
 	}
 
 	err = h.Withdrawal.Create(r.Context(), &wdl)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
-	writeAnswer(w, "withdrawal succeeded", http.StatusOK)
+	h.writeAnswer(w, "withdrawal succeeded", http.StatusOK)
 }
 
 // получение информации о выводе средств с накопительного счёта пользователем
@@ -220,18 +220,19 @@ func (h *HandlerData) withdrawalsHandler(w http.ResponseWriter, r *http.Request)
 	usrID := user.GetUserIDFromRequest(r)
 	withdrawals, err := h.Withdrawal.FindForUser(r.Context(), usrID)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	if len(withdrawals) < 1 {
-		writeAnswer(w, "no orders", http.StatusNoContent)
+		h.writeAnswer(w, "no orders", http.StatusNoContent)
 		return
 	}
 
 	w.Header().Set("Content-Type", "application/json")
 	err = json.NewEncoder(w).Encode(withdrawals)
 	if err != nil {
-		writeError(w, err.Error(), http.StatusInternalServerError)
+		h.writeError(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 }
